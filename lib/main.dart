c165656adfe8cc66653426e66f2cfffc1f9bc972
DarelJamesNarvasa <dareljames.narvasa@g.msuiit.edu.ph>
2024-10-14 23:34:56 +0800

    Theme: Promotion of Human Well-Being

--- lib/main.dart
+++ lib/main.dart
@@ -129,7 +129,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo', // App title
       theme: ThemeData(
         // This is the theme of your application.
         //
